9d3da61ecf3194e90bf284acf6e7b6843e027975
matt2112 <matt_lewis@live.co.uk>
2016-08-17 20:01:44 +0100

    setup starting template

---
+++ src/js/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "../../styles/styles.scss";
+
+export default class Layout extends React.Component {
+
+    render() {
+        return (
+            <div id="main">
+                <h1 id="title">Collaboration Test</h1>
+            </div>
+        );
+    }
+}
